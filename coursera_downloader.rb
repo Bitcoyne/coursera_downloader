62a1a114fe1bfab21540609504865e1517dc4c3a
Stephen Coyne <stephenjcoyne@gmail.com>
2014-07-02 20:22:17 +0100

    Updated for site changes

--- coursera_downloader.rb
+++ coursera_downloader.rb
@@ -1,14 +1,14 @@
 require 'Nokogiri'
 
 PAGE = Nokogiri::HTML(open('video_page.html'))
-DIRETORY  = './' + PAGE.css('.course-topbanner-name.coursera-university-color').text.strip.gsub!(' ', '_')
+DIRECTORY = PAGE.css(".course-lecture-item-resource a")[1]['href'].split('/')[3]
 
 def main
-  `mkdir #{DIRETORY}`
+  `mkdir #{DIRECTORY}`
   downloaded = File.open('download_log.txt', 'rb').read
 
   links.each do |link|
-    `wget --load-cookies cookies.txt --tries=25 '#{link.last}' -O '#{DIRETORY + '/' + link.first.gsub!(' ', '_')}'` if not downloaded.include?(link.last)
+    `wget --load-cookies cookies.txt --tries=25 '#{link.last}' -O '#{DIRECTORY + '/' + link.first.gsub!(' ', '_')}'` if not downloaded.include?(link.last)
     write_txt('download_log.txt', link.last)
   end
 end
@@ -16,7 +16,9 @@ end
 def links
   page_links = []
   PAGE.css(".course-lecture-item-resource a").each do |video|
-    page_links << [video.text.strip, video['href']]
+    if video['href'].include? '.mp4'
+      page_links << [video.text.strip, video['href']]
+    end
   end
   page_links
 end
